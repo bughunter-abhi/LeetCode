1a67168b765450653fb13522663081dce3fcd45f
Abhishek <abhishek.singh.bhandari488@gmail.com>
2025-12-18 10:16:05 +0530

    Time: 2 ms (26.87%), Space: 60.1 MB (76.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,11 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let Length = s.length;
-    let halfLength = Math.floor(Length / 2);
-    for(let i = 0; i < halfLength; i++){
-        let temp = s[i];
-        s[i] = s[Length - i - 1];
-        s[Length - i - 1] = temp;
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right){
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
     }
 };
